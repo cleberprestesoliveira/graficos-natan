df10bb199413d04e8b3da8c2750aa3f9a7d62985
cleberprestesoliveira <107960108+cleberprestesoliveira@users.noreply.github.com>
2024-10-08 16:40:29 -0300

    Update informacoesGlobais.js

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -3,7 +3,7 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function vizualizarInformacoesGlobais(){
     const res = await fetch(url)
     const dados = await res.json()
-    const pessoasConectadas = (dad90s.total_pessoas_conectadas / 1e9)
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
     const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
     const horas = parselnt(dados.tempo_medio)
     const minutos = Math.round((dados.tempo_medio - horas) * 100)
