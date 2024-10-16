19094f56d34de44a7e1acc3a2072467cca73464e
cleberprestesoliveira <107960108+cleberprestesoliveira@users.noreply.github.com>
2024-10-16 11:18:26 -0300

    Update common.js

--- common.js
+++ common.js
@@ -1,3 +1,4 @@
+Pouco armazenamento … 8% restantes do seu armazenamento individual de 15 GB. Para evitar interrupções, libere espaço ou fale com seu administrador.
 const getCSS=(variavel) =>{
     const bodyStyles= getComputedStyle(document.body)
     return bodyStyles.getPropertyValue(variavel)
