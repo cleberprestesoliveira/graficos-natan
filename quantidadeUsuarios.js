76c3d47b14f387ebcae4a0c6b421b1dd7434460b
cleberprestesoliveira <107960108+cleberprestesoliveira@users.noreply.github.com>
2024-10-16 11:34:26 -0300

    linha 53

--- quantidadeUsuarios.js
+++ quantidadeUsuarios.js
@@ -50,7 +50,7 @@ async function quantidadeUsuarioPorRede(){
     }
     const grafico=document.createElement('div')
     grafico.className='grafico'
-    document.getElementById('grafico-container').appendChild(grafico)
+    document.getElementById('graficos-container').appendChild(grafico)
     Plotly.newPlot(grafico,data,laytout)
 }
 quantidadeUsuarioPorRede()
